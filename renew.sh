7e62f1016a1c39194e698ad186eef0a88e0908b9
cqlp1314 <72656017+cqlp1314@users.noreply.github.com>
2021-04-30 10:43:59 +0800

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 result=$(/usr/bin/certbot renew --pre-hook "systemctl stop caddy" --post-hook "systemctl start caddy")
+send() {
+/root/mine/telegram/send_message.sh $1
+}
 if [[ $result == *"succeeded"* ]];then
-  /root/mine/telegram/send_message.sh "$1 domain certificate renew succeeded!" 
-  sudo cp /etc/letsencrypt/live/$domain/* /usr/local/etc/xray/ssl/     # pay attention to domain ,may need change
-  chmod 644 /usr/local/etc/xray/ssl/*
+  send "$1 domain certificate renew succeeded!" 
+  sudo cp /etc/letsencrypt/live/$domain/* /usr/local/etc/xray/ssl/  || export fault=1   # pay attention to domain ,may need change
+  chmod 644 /usr/local/etc/xray/ssl/* || export fault=1
   systemctl restart xray
+  result=$(systemctl status xray.service | awk '/failed/{print 1}')
+  [[ -z $result ]] || export fault=1
+  [[ $fault = 1 ]] && send "$1 domain certificate renew succeeded,but something run after it failed" || send "$1 domain certificate renew succeeded!" 
 elif [[ $result == *"No renewals"* ]];then
   /root/mine/telegram/send_message.sh "$1 domain certificate do not need renew!"
 else
