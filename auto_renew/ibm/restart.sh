6ca46479e46b332f8d1efd711ee899c0efae73f6
cqlp1314 <72656017+cqlp1314@users.noreply.github.com>
2021-05-02 21:37:13 +0800

    Update restart.sh

--- auto_renew/ibm/restart.sh
+++ auto_renew/ibm/restart.sh
@@ -5,6 +5,7 @@ region=${inf_array[0]}
 account=${inf_array[1]}
 passwd=${inf_array[2]}
 appname=${inf_array[3]}
+platform=${inf_array[4]}
 }
 ibm_restart() {
 /usr/local/bin/cf l -a https://api.${region}.cf.cloud.ibm.com login -u ${account} -p ${passwd} && /usr/local/bin/cf rs ${appname}
