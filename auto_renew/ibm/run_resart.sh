d69e873c9fc005069fd0adc59b1d708e27c28b87
cqlp1314 <72656017+cqlp1314@users.noreply.github.com>
2021-05-02 20:38:46 +0800

    Update run_resart.sh

--- auto_renew/ibm/run_resart.sh
+++ auto_renew/ibm/run_resart.sh
@@ -1,1 +1,26 @@
-echo 1
+#!/bin/bash
+cd ~/mine/ibm/
+source restart.sh
+terminate="trojan-go"
+forever="trojan-r"
+files=$(find . -name "inf*")
+files=($files)
+length=${#files[@]}
+day=$(date +"%d")
+#number=$(($day % 10))
+for i in $(seq 1 $length);do
+ibm_info inf${i}.txt
+result=$(ibm_restart)
+#echo "result :$result"
+
+if [[ "$result" =~ "running" ]]
+then
+ if  [[ $day = 01 ]]
+ then
+   /home/ubuntu/mine/telegram/send_message.sh "ibm${i} restart succeeded!Account:${account},Appname:${appname} App:${!appname}"
+ fi
+else
+ /home/ubuntu/mine/telegram/send_message.sh "ibm${i} restart failed!Account:${account},Appname:${appname} App:${!appname}"
+fi
+sleep 60
+done
